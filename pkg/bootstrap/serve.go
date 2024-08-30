0cec82f603ab3b1d2b29109cc4bcbb49027eae2a
Tahmurath <hamidreza.tahmooresi@gmail.com>
2024-08-30 17:04:14 +0330

    10 static files

--- pkg/bootstrap/serve.go
+++ pkg/bootstrap/serve.go
@@ -4,12 +4,15 @@ import (
 	"resedist/pkg/config"
 	"resedist/pkg/html"
 	"resedist/pkg/routing"
+	"resedist/pkg/static"
 )
 
 func Serve() {
 	config.Set()
 
 	routing.Init()
+
+	static.LoadStatic(routing.GetRouter())
 
 	html.LoadHTML(routing.GetRouter())
 
