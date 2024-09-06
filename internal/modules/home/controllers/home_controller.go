8876f18fad5eb81fb6164cffe1c59e695f7001a0
Tahmurath <hamidreza.tahmooresi@gmail.com>
2024-09-06 14:52:19 +0330

    19 html bind

--- internal/modules/home/controllers/home_controller.go
+++ internal/modules/home/controllers/home_controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"net/http"
+	"resedist/pkg/html"
 
 	"github.com/gin-gonic/gin"
 
@@ -21,11 +22,13 @@ func New() *Controller {
 }
 
 func (controller *Controller) Index(c *gin.Context) {
-	//html.Render(c, http.StatusOK, "modules/home/html/home", gin.H{
-	//	"title": "Home Page",
-	//})
-	c.JSON(http.StatusOK, gin.H{
+	html.Render(c, http.StatusOK, "modules/home/html/home", gin.H{
+		"title":    "Home Page",
 		"featured": controller.articleService.GetFeaturedArticles(),
 		"stories":  controller.articleService.GetStoriesArticles(),
 	})
+	//c.JSON(http.StatusOK, gin.H{
+	//	"featured": controller.articleService.GetFeaturedArticles(),
+	//	"stories":  controller.articleService.GetStoriesArticles(),
+	//})
 }
