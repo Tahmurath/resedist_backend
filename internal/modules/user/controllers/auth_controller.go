0c2c2eb3126049fe64906d81ddf20303b483dd6c
Tahmurath <hamidreza.tahmooresi@gmail.com>
2024-09-27 15:20:54 +0330

    24 register form validation

--- internal/modules/user/controllers/auth_controller.go
+++ internal/modules/user/controllers/auth_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"resedist/internal/modules/user/requests/auth"
 	"resedist/pkg/html"
 )
 
@@ -20,6 +21,18 @@ func (controller *Controller) Register(c *gin.Context) {
 }
 
 func (controller *Controller) HandleRegister(c *gin.Context) {
+	// validate
+	var request auth.RegisterRequest
+	// This will infer what binder to use depending on the content-type header.
+	if err := c.ShouldBind(&request); err != nil {
+		c.Redirect(http.StatusFound, "/register")
+		return
+	}
+	// create user
+
+	// check errors
+	// redirect
+
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Register Done",
 	})
